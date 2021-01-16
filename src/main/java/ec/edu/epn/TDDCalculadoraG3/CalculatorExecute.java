50e7dd2692eafba6934d56103d924c4091801e0e
andrespalma02 <69227381+andrespalma02@users.noreply.github.com>
2021-01-16 14:40:00 -0500

    -Creacion de las clase CalculatorExecute para la ejecucion del programa

---
+++ src/main/java/ec/edu/epn/TDDCalculadoraG3/CalculatorExecute.java
@@ -0,0 +1,17 @@
+package ec.edu.epn.TDDCalculadoraG3;
+
+public class CalculatorExecute {
+    public static void main(String[] args) {
+        System.out.println("== Calculator Execute ==");
+
+        Calculator c = new Calculator();
+        int addition = c.addition(4,7);
+        System.out.println("c.addition(4,7) = " + addition);
+
+        int subtraction = c.subtraction(7,1);
+        System.out.println("c.subtraction(7,1) = " + subtraction);
+
+        System.out.println("== It is a change !! ==");
+
+    }
+}
